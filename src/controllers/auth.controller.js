601cfb96f213947a0da272bb1e428452349a01bf
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-13 13:08:24 -0500

    [+] auth.routes: Define validaciones de campos [+] auth.controller: Valida resultado de errores de validacion

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,7 +1,19 @@
 const { response, request } = require( 'express' );
+const { validationResult } = require('express-validator');
 
 
 const createUser = ( req = request, res = response ) => {
+    const errors = validationResult( req );
+    console.log( errors );
+
+    // Verificamos si la lista de errores no esta vacia
+    if( ! errors.isEmpty() ) {
+        return res.status( 400 ).json({
+            ok: false,
+            errors: errors.mapped()
+        });
+    }
+
     return res.json({
         path: '/register',
         msg: 'Registro de usuario'
@@ -9,6 +21,18 @@ const createUser = ( req = request, res = response ) => {
 }
 
 const authenticateUser = ( req = request, res = response ) => {
+    const errors = validationResult( req );
+    console.log( errors );
+
+    // Verificamos si la lista de errores no esta vacia
+    if( ! errors.isEmpty() ) {
+        return res.status( 400 ).json({
+            ok: false,
+            errors: errors.mapped()
+        });
+    }
+
+
     return res.json({
         path: '/login',
         msg: 'Login de usuario'
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,37 @@
 const { Router } = require( 'express' );
 const { createUser, authenticateUser, renewToken } = require('../controllers/auth.controller');
+const { check } = require('express-validator');
 
 const router = Router();
 
 
-router.post( '/register', createUser );
-router.post( '/login', authenticateUser );
+router.post( 
+    '/register', 
+    [
+        check( 'name', '\'name\' doesn\'t exists').exists(),
+        check( 'email', 'Email is required' )
+            .trim()
+            .isEmail(),
+        check( 'password', 'Password is required' )
+            .trim()
+            .isLength({ min: 8 })
+            .withMessage( 'Must be at least 8 chars long' ),
+    ], 
+    createUser 
+);
+router.post( 
+    '/login', 
+    [
+        check( 'email', 'Email is required' )
+            .trim()
+            .isEmail()
+            .withMessage( 'Must be at least 8 chars long' ),
+        check( 'password', 'Password is required' )
+            .trim()
+            .isLength({ min: 8 }),
+    ],
+    authenticateUser 
+);
 router.get( '/renew', renewToken );
 
 
