05d6e635ef7d37c8a524b3e3c1df3a63122c6a7f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-13 14:29:30 -0500

    [+] User.model

---
+++ src/models/User.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require( 'mongoose' );
+
+const UserSchema = new Schema(
+    {
+        name: {
+            type: 'string',
+            required: true,
+        },
+        email: {
+            type: 'string',
+            required: true,
+            unique: true
+        },
+        password: {
+            type: 'string',
+            required: true,
+        }
+    },{
+        timestamps: true
+    }
+);
+
+
+module.exports = model( 'User', UserSchema );
