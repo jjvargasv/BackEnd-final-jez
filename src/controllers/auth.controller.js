a32dcfec01c5ede4ce0a8fc95fdcfba308dd24f3
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-13 11:11:38 -0500

    [+] auth.controller: Implementar tipos a res, req desde una importacion de Express

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,18 +1,21 @@
-const createUser = ( req, res ) => {
+const { response, request } = require( 'express' );
+
+
+const createUser = ( req = request, res = response ) => {
     return res.json({
         path: '/register',
         msg: 'Registro de usuario'
     });
 }
 
-const authenticateUser = ( req, res ) => {
+const authenticateUser = ( req = request, res = response ) => {
     return res.json({
         path: '/login',
         msg: 'Login de usuario'
     });
 }
 
-const renewToken = ( req, res ) => {
+const renewToken = ( req = request, res = response ) => {
     return res.json({
         path: '/renew',
         msg: 'Renovar token'
