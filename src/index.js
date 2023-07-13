855d82fc06b35cbc3941cdb77a08469d86ff1687
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-13 10:12:35 -0500

    [+] index.js: Lanza Servidor en puerto 4000 usando Node/Express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log( 'Hola desde Node' );
+const 
+    express = require( 'express'  ),
+    app = express(),
+    PORT = 4000;
+
+app.listen( PORT, () => {
+    console.log( `Servidor lanzado en http://localhost:${ PORT }` );
+});
