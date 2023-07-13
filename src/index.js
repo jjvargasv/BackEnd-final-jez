ee461da0cefcb38bac19992f961a3e05d587c289
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-13 14:14:13 -0500

    [+] mongo.config: Configura conexion a MongoDB usando Mongoose

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+const { dbConnection } = require('./config/mongo.config');
 const { route } = require('./routes/auth.routes');
 
 require( 'dotenv' ).config();
@@ -15,6 +16,8 @@ app.use( express.static( 'public') );   // Directorio público
 app.use( cors() );              // Cross-Origin-Resources-Sharing
 app.use( express.json() );      // Lectura Parseo del body
 app.use( '/api/auth', require( './routes/auth.routes' ) );
+
+dbConnection();                 // Invoca la inicializacion de la base de datos
 
 app.listen( PORT, () => {
     console.log( `Servidor lanzado en http://localhost:${ PORT }` );
