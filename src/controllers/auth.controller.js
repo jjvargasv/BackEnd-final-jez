6475a0a59e03b114e2a3b6d373a224d042bb00fd
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-14 11:09:49 -0500

    [+] auth.controller: Registra usuario

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,12 +1,51 @@
 const { response, request } = require( 'express' );
+const User = require('../models/User');
 
 
-const createUser = ( req = request, res = response ) => {
+const createUser = async ( req = request, res = response ) => {
 
-    return res.json({
-        path: '/register',
-        msg: 'Registro de usuario'
-    });
+    const { name, email, password } = req.body;
+
+    try {
+        // 1. Verificar que existe el 'email'
+        const userFound = await User.findOne({ email });
+
+        if( userFound ) {
+            return res.status( 400 ).json({
+                ok: false,
+                path: '/register',
+                msg: 'El usuario ya existe'
+            });
+        }
+
+        // 2. Crear el nuevo usuario
+        const dbUser = new User( req.body );
+
+        // 2. Encriptar la contraseña
+
+        // 3. Generar el JWT, como metodo de autenticación pasiva
+
+        // 4. Registrar usuario en la base de datos
+        const newUser = dbUser.save();
+
+        // 5. Responder al cliente
+        console.log( newUser );
+        res.status( 201 ).json({
+            ok: true,
+            path: '/register',
+            msg: 'Usuario creado existosamente',
+            user: newUser
+        }); 
+    } 
+    catch ( error ) {
+        console.log( error );
+        return res.status( 500 ).json({
+            ok: false,
+            path: '/register',
+            msg: 'Error al registrar usuario'
+        });    
+    }
+
 }
 
 const authenticateUser = ( req = request, res = response ) => {
