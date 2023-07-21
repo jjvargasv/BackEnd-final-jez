1e35ef2407798860f15abd70c473b287fcf142e0
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-21 10:29:21 -0500

    [+] product.services: CRUD contra DB usando Mongoose

---
+++ src/services/product.service.js
@@ -0,0 +1,35 @@
+import ProductModel from "../models/products.model";
+
+
+const insertProduct = async ( product ) => {
+    return await ProductModel.create( product );
+}
+
+const getAllProducts = async () => {
+    return await ProductModel.find({});
+}
+
+const getProductById = async ( productId ) => {
+    return await ProductModel.findOne({ _id: productId });
+}
+
+const removeProductById = async ( productId ) => {
+    return await ProductModel.findOneAndRemove({ _id: productId });
+}
+
+const updateProductById = async ( productId, updateProduct ) => {
+    return await ProductModel.findOneAndUpdate( 
+        { _id: productId },     // Id del documento que deseamos actualizar
+        updateProduct,          // El documento por el que vamos a actualizar 
+        { new: true }           // Configuracion para el comando Update
+    );
+}
+
+
+export {
+    insertProduct,
+    getAllProducts,
+    getProductById,
+    removeProductById,
+    updateProductById
+}
