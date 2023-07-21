375e00a57105ce5b76afcd778b8f5c0171ddb8e4
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-21 09:58:05 -0500

    [+] Product.model

---
+++ src/models/Products.js
@@ -0,0 +1,52 @@
+/** La estructura que definamos aqui se vera reflejada en nuestra base de datos */
+import { Schema, model } from "mongoose";
+
+
+/** Define estructura de datos en la base de datos requerida por Mongoose*/
+const productSchema = new Schema(
+    // Objeto principal definira atributos del modelo
+    {
+        name: {
+            type: String,
+            required: true
+        },
+        price: {
+            type: Number,
+            required: false
+        },
+        urlImage: {
+            type: String,
+            required: false
+        },
+        description: {
+            type: String
+        },
+        quantity: {
+            type: Number,
+            required: true
+        },
+        ranking: {
+            type: Number,
+            default: 0
+        },
+        /** Atributos vinculantes */
+        category: {
+            type: String,
+            default: 'Uncategorized'
+        }, 
+        userId: {
+            type: String,
+            required: true
+        }
+    },
+    // Definira configuraciones que se pueden aplicar en Mongoose para ese objeto
+    {
+        timestamps: true
+    }
+);
+
+/** Define el Modelo a partir de la estructura requerida por Mongoose */
+const ProductModel = model( 'Products', productSchema  );
+
+
+export default ProductModel;
